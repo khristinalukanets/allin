15ff0c63f9208716f4040acbe8eec4407eec9a74
khlukanets <khristina.lukanets@hevelian.com>
2020-10-18 22:46:54 +0300

    KLU: Add random joke api on home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "../../axios";
+import Card from '@material-ui/core/Card';
+import classes from './Home.module.scss';
 
-const home = (props) => (
-	<div>
-		Home
-	</div>
-);
+const Home = () => {
+	const [joke, setJoke] = useState("");
 
-export default home;
+	useEffect(() => {
+		axios.get('https://sv443.net/jokeapi/v2/joke/Any?type=single')
+			.then(res => {
+				setJoke(res.data);
+			})
+			.catch(() => {
+
+			});
+	}, []);
+
+	if (joke) {
+		return <Card className={classes.JokeCard}>
+			<h2>Joke of a day {joke.date}</h2>
+			<p>{joke.joke}</p>
+		</Card>;
+	}
+	return (
+		<div>
+			No jokes for today.
+		</div>
+	);
+};
+
+export default Home;
